92bddf60cf5d7f01ace27b1e25874bea49f86c15
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2025-02-13 10:24:22 -0500

    Cambios en las direcciones

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -8,7 +8,7 @@ user_routes = Blueprint("user_routes", __name__)
 # Definir las rutas
 
 #Ruta para crear un usuario
-@user_routes.route("/users", methods=["POST"])
+@user_routes.route("/users/", methods=["POST"])
 def create_user():
     try:
         return create_user_controller(request.get_json())
@@ -16,7 +16,7 @@ def create_user():
         return jsonify({"error": str(e)}), 400
 
 #Ruta para obtener todos los usuarios
-@user_routes.route("/users", methods=["GET"])
+@user_routes.route("/users/", methods=["GET"])
 def get_users():
     try:
         return get_all_users_controller()
@@ -24,7 +24,7 @@ def get_users():
         return jsonify({"error": str(e)}), 400
 
 #Ruta para obtener un usuario por su código
-@user_routes.route("/users/<string:code>", methods=["GET"])
+@user_routes.route("/users/<string:code>/", methods=["GET"])
 def get_user(code):
     try:
         return get_user_by_code_controller(code)
@@ -32,7 +32,7 @@ def get_user(code):
         return jsonify({"error": str(e)}), 400
 
 #Ruta para actualizar un usuario
-@user_routes.route("/users/<string:code>", methods=["PUT"])
+@user_routes.route("/users/<string:code>/", methods=["PUT"])
 def update_user(code):
     try:
         return update_user_controller(code, request.get_json())
@@ -40,7 +40,7 @@ def update_user(code):
         return jsonify({"error": str(e)}), 400
 
 #Ruta para eliminar un usuario
-@user_routes.route("/users/<string:code>", methods=["DELETE"])
+@user_routes.route("/users/<string:code>/", methods=["DELETE"])
 def delete_user(code):
     try:
         return delete_user_controller(code)
@@ -48,7 +48,7 @@ def delete_user(code):
         return jsonify({"error": str(e)}), 400
 
 #Ruta para obtener los usuarios deshabilitados
-@user_routes.route("/users/disabled", methods=["GET"])
+@user_routes.route("/users/disabled/", methods=["GET"])
 def get_users_disabled():
     try:
         return get_users_dissabled_controller()
@@ -56,7 +56,7 @@ def get_users_disabled():
         return jsonify({"error": str(e)}), 400
 
 #Ruta para habilitar un usuario
-@user_routes.route("/users/enable/<string:code>", methods=["PUT"])
+@user_routes.route("/users/enable/<string:code>/", methods=["PUT"])
 def enable_user(code):
     try:
         return enable_user_controller(code)
