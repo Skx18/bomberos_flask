2a1fd30a587a894a0ee168af82df6eff8cd0abd7
juanpalacios20 <juan.palacios.casanova@correounivalle.edu.co>
2025-04-14 17:17:31 -0500

    Actualizar configuración de la base de datos y credenciales de correo para usar variables de entorno

--- app.py
+++ app.py
@@ -31,17 +31,17 @@ def create_app():
     app = Flask(__name__)
 
     # Configuración de la base de datos
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = "tu_secreto"
+    app.config['JWT_SECRET_KEY'] = os.environ.get("TU_SECRETO")
 
     #Configuración correo
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_USERNAME'] = 'soportedeaplicacion2025@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'twie ogxx rmhq mdex'
+    app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
+    app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 
 
     cloudinary.config(
